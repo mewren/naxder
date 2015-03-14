3c3a8797c7b6b98371b6a0859fd08536f19202ca
Jennifer Nolan <jen.nolan@ca.ibm.com>
2015-03-14 15:00:06 -0400

    updates from Mama's computer

---
+++ magicTreehouseSeries/goodreads_export_files/bde.js
@@ -0,0 +1,9 @@
+// Qualaroo for goodreads.com
+// (C) 2014 Qualaroo. All rights reserved.
+// qualaroo.com
+
+//$ site: 45902, generated: 2014-10-16 11:17:35 UTC
+//$ type: base, rev. ecf67886 (deploy)
+//$ client: 2.0.7
+
+//Your Qualaroo account is suspended or disabled.  Please see your dashboard at https://app.qualaroo.com (or email support@qualaroo.com).
